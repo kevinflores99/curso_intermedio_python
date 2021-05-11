a56c52b6a412afe236558d7caf781ac805207a24
Kevin Flores <kevinflores1999@hotmail.com>
2021-05-10 22:08:25 -0500

    High order functions

--- Kevin_pyi/dict_comprehensions.py
+++ Kevin_pyi/dict_comprehensions.py
@@ -1,15 +1,19 @@
 def run():
     # my_dict = {}
     # for i in in range(1, 101):
-    #     my_dict[i] = i**3
+    #     if i % 3 != 0:
+    #         my_dict[i] = i**3
 
     # print(my_dict)
 
     # dic = {i: i**3 for i in range(1, 101)}
     # print(dic)
 
-    dic = {i: i for i in range(1, 101) if i % 3 != 0}
-    print(dic)
+    # dic = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    # print(dic)
+
+    n_dic = {i: round(i**(0.5), 2) for i in range(1, 1001)}
+    print(n_dic)
 
 
 if __name__ == "__main__":
