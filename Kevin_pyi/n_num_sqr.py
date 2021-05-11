a0679eb6c02822f45aebd4d36ef0c2db0ecbb8a4
Kevin Flores <kevinflores1999@hotmail.com>
2021-05-10 21:13:39 -0500

    List Excercises

--- Kevin_pyi/n_num_sqr.py
+++ Kevin_pyi/n_num_sqr.py
@@ -1,10 +1,17 @@
 def run():
-    n_num = list(range(11))
-    sqr = []
-    for num in n_num:
-        n = num**2
-        sqr.append(n)        
-    print(sqr)    
+    # num_n = []
+    # for num in range(1,11):
+    #     if num % 3 != 0:
+    #         num_n.append(num**2)
+
+    # print(num_n)
+
+    # squares = [i**2 for i in range(1,101) if i % 3 != 0]
+    # print(squares)
+
+    mult = [i for i in range(1, 100000) if i %
+            4 == 0 and i % 6 == 0 and i % 9 == 0]
+    print(mult)
 
 
 if __name__ == '__main__':
