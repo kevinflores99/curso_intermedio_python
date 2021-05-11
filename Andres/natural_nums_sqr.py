bbe14cb2fd26b14be75bfce2182804ebe8347823
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-05-10 20:49:33 -0500

    La cague

---
+++ Andres/natural_nums_sqr.py
@@ -0,0 +1,6 @@
+def main():
+    squared_nums = [x**2 for x in range(1,101)]
+    print(squared_nums)
+
+if __name__ == "__main__":
+    main()
---
+++ Kevin_pyi/n_num_sqr.py
@@ -0,0 +1,11 @@
+def run():
+    n_num = list(range(11))
+    sqr = []
+    for num in n_num:
+        n = num**2
+        sqr.append(n)        
+    print(sqr)    
+
+
+if __name__ == '__main__':
+    run()
