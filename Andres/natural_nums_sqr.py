b36c941fd5850344b9b338a0427c870f9aa33423
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-05-10 21:13:00 -0500

    Added the challenge of the list_comprenhensions

--- Andres/natural_nums_sqr.py
+++ Andres/natural_nums_sqr.py
@@ -2,5 +2,10 @@ def main():
     squared_nums = [ x**2 for x in range(1,101) if x%3 != 0]
     print(squared_nums)
 
+    print("======================================================")
+    # List of nums multiples of 4,6,9 
+    multiples = [ x for x in range(1,10000) if x%4==0 and x%6==0 and x%9==0]
+    print(multiples)
+
 if __name__ == "__main__":
     main()
