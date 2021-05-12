c47b90a1f7b64610abed98db190c218f725fb8ac
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-05-12 01:16:46 -0500

    Starting the hangman project

---
+++ Andres/hangman_game.py
@@ -0,0 +1,15 @@
+from random import randint
+
+def read_file():
+    pass
+
+def get_word():
+    # random_number = randint()
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
