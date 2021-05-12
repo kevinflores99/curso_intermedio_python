3b461015547591b09001eaa95f0734f04da324e1
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-05-11 22:22:42 -0500

    Added the challenge of try except

--- Andres/debugging.py
+++ Andres/debugging.py
@@ -1,11 +1,23 @@
 def divisors(num):
-    return [i for i in range(1, num+1) if num%i==0]
+    try:
+        if num <= 0:
+            raise ValueError("Use only numbers higher than 0")
+        return [i for i in range(1, num+1) if num%i==0]
+
+    except ValueError as ve:
+        print(ve)
+        return
 
 
 def main():
-    num = int(input("Type a number: "))
-    print(divisors(num))
-    print("Program finished")
+    
+    try:
+        num = int(input("Type a number: "))
+        print(divisors(num))
+        print("Program finished")
+    except ValueError:
+        print('Just numbers are accepted as input')
+
 
 
 if __name__ == "__main__":
